13d9af9f5a640faf711989800bfde0d49793aee7
Wade Norris <swat510@gmail.com>
2011-10-30 15:48:36 -0700

    (Wade) Extracted javacscript functions to utility files to follow specification. Adjusted appearance on help page and language for mutliple examples.

---
+++ srs/mobile/javascript/CommunicationUtility.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+
+function getUrlVars()
+{
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = unescape(hash[1]);
+    }
+    return vars;
+}
