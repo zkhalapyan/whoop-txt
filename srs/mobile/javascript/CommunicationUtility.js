63fc22439967bc09ba040cc77f18045e05adccfd
Wade Norris <swat510@gmail.com>
2011-11-02 23:09:52 -0700

    (Wade) Added AJAX function to javascript communication utility.

--- srs/mobile/javascript/CommunicationUtility.js
+++ srs/mobile/javascript/CommunicationUtility.js
@@ -12,3 +12,43 @@ function getUrlVars()
     }
     return vars;
 }
+
+function AJAXRequest(url, callbackFunction)
+{
+	var xmlhttp;
+	if (window.XMLHttpRequest)
+	{// code for IE7+, Firefox, Chrome, Opera, Safari
+		xmlhttp=new XMLHttpRequest();
+	}
+	else
+	{// code for IE6, IE5
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	
+	xmlhttp.open("GET", url, true);
+	
+	xmlhttp.onreadystatechange=function()
+	{
+		if (xmlhttp.readyState==4)
+		{
+			if(xmlhttp.status==200)
+			{
+				// Succesfull request reponse from server
+				if(callbackFunction)
+				{
+					var stringResponse = xmlhttp.responseText;
+					var JSONResponse = eval('(' + stringResponse + ')');
+					
+					callbackFunction(JSONResponse);
+				}
+			}
+			else
+			{
+				console.log("Error in AJAX Request! Server returned HTTP Status " + xmlhttp.status);
+			}
+		}
+	};
+	
+	xmlhttp.send();
+	
+}
