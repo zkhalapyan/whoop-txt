b7287b957d42024a00f86704a04c988a5961bba7
Wade Norris <swat510@gmail.com>
2011-11-30 11:48:08 -0800

    (Wade) Update file structure and pulled out more common API calls to the WhoopTxt Utility.

---
+++ srs/mobile/js/WhoopTxtUtility.js
@@ -0,0 +1,97 @@
+// JavaScript Document
+
+function deleteMessage (id) {
+	AJAXRequest("https://rocking-apps.com/whooptxt/api/api.php?action=mark_message&deleted=true&opened=true&important=false&message_id="+id, function (response) {
+		
+		if(response.status != "success")
+		{
+			handleError(response);
+			return;
+		}
+		
+		window.location = "messages.html";
+		
+	});
+};
+
+function getMessages(callback) {
+	AJAXRequest("https://rocking-apps.com/whooptxt/api/api.php?action=get_messages", function (response) {
+	
+		if(response.status != "success")
+		{
+			handleError(response);
+			return;
+		}
+		
+		callback(response.data.messages);
+	});
+};
+
+function getMessageById(id, callback) {
+	getMessages(function (messages) {
+		
+		for (index in messages)
+		{
+			if(messages[index].id == id)
+			{
+				callback(messages[index]);
+			}
+		}
+		
+	});
+};
+
+function getTokens(callback) {
+	AJAXRequest("https://rocking-apps.com/whooptxt/api/api.php?action=get_tokens", function (response) {
+		callback(response.data.tokens);
+	});
+};
+
+function shareWhoop(messageBody, tokenIds, lon, lat) {
+	var url = "https://rocking-apps.com/whooptxt/api/api.php?action=send_message";
+	url += "&message=" + messageBody;
+	url += "&token_ids=" + tokenIds;
+	url += "&lon=" + lon;
+	url += "&lat=" + lat;
+	
+	AJAXRequest(url, function (response) {
+		window.location = "messages.html";
+	});
+};
+
+function reshareMessage(id) {
+	getMessageById(id, function (msg) {
+	
+		var tokens = msg.tokens;
+		var token_names = "";
+		
+		for (index in tokens)
+		{
+			token_names = tokens[index].name + ", ";
+		}
+		
+		if(token_names.length != 0)
+		{
+			token_names = token_names.substr(0, token_names.length-2);
+		}
+		
+		var msg_text = "Originally shared by " + msg["author_name"] + ": " + msg.text;
+		
+		window.location = "share.html?tokenNames=" + token_names + "&text=" + msg_text;
+	
+	});
+};
+
+function getLocation(callback) {
+	mwf.touch.geolocation.getPosition(
+		function(pos){
+			var lat = pos['latitude'];
+			var lon = pos['longitude'];
+			
+			callback(lat, lon);
+		}, 
+		function(err){
+			Console.log("Error: " + err);
+		}
+	);
+};
---
+++ srs/mobile/js/share.php
@@ -0,0 +1,75 @@
+<?PHP
+
+require_once (dirname(dirname(__FILE__))."/api/auth/auth.php");
+require_once (dirname(dirname(__FILE__))."/api/config/ConfigFactory.class.php");
+
+?>
+
+<!DOCTYPE html>
+<html>
+   
+    <head>
+        <title> Welcome to Whoop-Txt </title>
+           
+        <!-- IMPORT JQUERY LIBRARY HOSTED BY GOOGLE -->
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script> 
+        
+        
+        <link rel="stylesheet" href="../javascript/css/app_styles.css" type="text/css" />   
+        <link rel="stylesheet" href="../javascript/css/fb_styles.css" type="text/css" />    
+        
+
+<script type="text/javascript">
+        function ChangeSize (){
+            var area_size = document.getElementById ("text_area");
+            area_size.style.height = "100px";
+        }
+
+       
+</script>
+  
+<script language="javascript" type="text/javascript" src="../javascript/js/actb.js"></script>
+<script language="javascript" type="text/javascript" src="../javascript/js/common.js"></script>
+<script>
+        var customarray=new Array('Nearby','My Groups','UCLA friends','TMNT','Whoop'); <!-- where  all tags go -->
+        var custom2 = new Array('something','randomly','different');
+</script>
+
+        
+    </head>
+    
+    <body>
+        <?PHP include("../javascript/app_header.php"); ?>
+        <div id="bodyContent">
+        
+        <div align="center" id ="form_div">
+        <form class="form-full form-padded">
+            <h1>Share New Whoop</h1>
+            <label for="audience">Tags:</label>
+            <br/>
+            <input type="text" name="audience" id="audience" autocomplete="off" /> 
+            <br/>
+            <br/>
+            <label for="subject">Subject:</label>
+            <br/>
+            <input type="text" name="subject" id="subject" autocomplete="off"/>
+            <br/>
+            <br/>
+            <label for="body">Message:</label>
+            <br/>
+            <textarea id ="text_area"onfocus="ChangeSize(this);"></textarea> 
+            <br/>
+            <input type="submit" name="submit" value="Whoop It" onclick="">
+        </form>
+        </div>
+            
+        <div>
+            
+       <script>
+            var obj = actb(document.getElementById('audience'),customarray);
+       </script>
+       
+        <?PHP include("../javascript/app_footer.php"); ?>
+    </body>
+    
+</html>
