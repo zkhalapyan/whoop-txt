4344e14c1c4ace9f42be8f852f21ccea7d8c3ff6
Anthony Balmeo <anthonybalmeo@gmail.com>
2011-12-01 00:21:04 -0800

    Revert "(Anthony Balmeo) Putting up Desktop code for Whoop-txt"
    This reverts commit ff0afd024c7fdd7a485fb51baf1bb628d47588d4.

--- srs/app/app_header.php
+++ srs/app/app_header.php
@@ -8,7 +8,7 @@
 
 	FB.init(
 			{
-     			//appId  : app_id,
+     			appId  : app_id,
      			status : true, // Check login status.
      			cookie : true, // Enable cookies to allow the server to access the session.
      			xfbml  : true  // Parse XFBML.
@@ -29,7 +29,7 @@
 	google_ad_height = 90;
 	//-->
 </script>
-<div id="header">
+ 
 <!-- GOOGLE AD DISPLAY -->
 <div align="center">
 	<script type="text/javascript"
@@ -37,20 +37,21 @@
 	</script>
 </div>
 
-    <div style="width: 728px; margin: auto;"> 
-        <a href ="index.php"><img style="float:left; height: 46px; width: 150px;" alt="" src="img/menu_left2.png"/></a>
+    <div style="fixed; width: 728px; margin: auto;">
+    <img style="float:left;" alt="" src="images/menu_left2.png"/>
         <ul id="menu">
             <li class="logo">
-               
                 
             </li>
+               <li><a href="messages.php" target="_self">Messages</a></li>
+               <li><a href="share.php" target="_self">Whoop It</a></li>
                <li><a href="groups.php" target="_self">Groups</a></li>
+               <li><a href="invitations.php" target="_self">Invitations</a></li>
                <li><a href="help.php" target="_self">Help</a></li> 
         </ul>
+        <img style="float:left;" alt="" src="images/menu_right.png"/>
     </div>
     <div style="float:none; clear:both;"></div>
-     
- </div>
 
 
 
--- srs/app/messages.php
+++ srs/app/messages.php
@@ -13,176 +13,23 @@ require_once (dirname(dirname(__FILE__))."/api/config/ConfigFactory.class.php");
            
         <!-- IMPORT JQUERY LIBRARY HOSTED BY GOOGLE -->
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script> 
+        
+        
         <link rel="stylesheet" href="css/app_styles.css" type="text/css" />   
         <link rel="stylesheet" href="css/fb_styles.css" type="text/css" />    
-        <script type="text/javascript" src="js/CommunicationUtility.js"></script>
-        <script language="javascript" type="text/javascript" src="js/actb.js"></script>
-        <script language="javascript" type="text/javascript" src="js/common.js"></script>
-        <script language="javascript" type="text/javascript" src="js/functions.js"></script>
-
-        <script type="text/javascript">
-            
-                AJAXRequest("https://rocking-apps.com/whooptxt/api/api.php?action=get_messages", function (response) {
-
-                        if(response.status != "success")
-                        {
-                                alert(response);
-                        }
-                        
-                        var messages = response.data.messages;
-                        for (index in messages)
-                        {
-                                
-                                addElement("elementList", messages[index]["author_name"], messages[index]["id"], messages[index]["text"]);
-                        }
-                });
-                
-                
-//                
-//        function delete_msg()
-//        {
-//
-//       // var del_url = "http://www.whoop-txt.com/api/mark_message?user_id="+user_id+"&message_id="+msg_id+"&read=true&delete=true";
-//        window.alert("This is an anchor test.");
-//        }
-                
-                
-        </script>
         
-   <!--SAMPLE TO TEXT-->
-   
-       <script>
-            var autoCompleteArray = new Array();
-            var autoCompleteIds = new Array();
-
-            AJAXRequest("https://rocking-apps.com/whooptxt/api/api.php?action=get_tokens", function (response) {
-
-            if(response.status != "success")
-            {
-                    alert("Connection to server failed!");
-                    return;
-            }
-
-            var tokens = response.data.tokens;
-            for (index in tokens)
-            {
-                    if(tokens[index].pending === false && tokens[index].active === true)
-                    {
-                            autoCompleteArray.push(tokens[index].name);
-                            autoCompleteIds.push(tokens[index].id);
-                    }
-            }
-
-            actb(document.getElementById('audience'), autoCompleteArray);
-
-            });
-
-        function submitWhoop () {
-
-                var tokenIds = "";
-                var tokenNames = document.getElementById('audience').value.split(', ');
-                for (var i=0; i<tokenNames.length; i++)
-                {
-                        for (index in autoCompleteArray)
-                        {
-                                if(tokenNames[i] == autoCompleteArray[index] || tokenNames[i] == " " + autoCompleteArray[index])
-                                {
-                                        tokenIds += autoCompleteIds[index] + ",";
-                                }
-                        }
-                }
-
-                if(tokenIds.length > 0)
-                {
-                        tokenIds = tokenIds.substr(0, tokenIds.length-1);
-                }
-
-                var messageBody = document.getElementById('text_area').value;
-
-
-                mwf.touch.geolocation.getPosition(
-                        function(pos){
-                                var lat = pos['latitude'];
-                                var lon = pos['longitude'];
-
-                                var url = "https://rocking-apps.com/whooptxt/api/api.php?action=send_message";
-                                url += "&message=" + messageBody;
-                                url += "&tokenIds=" + tokenIds;
-                                url += "&lon=" + lon;
-                                url += "&lat=" + lat;
-
-                                AJAXRequest(url, function (response) {
-                                        alert(JSON.stringify(response));
-                                });
-                        }, 
-                        function(err){ alert("Err:"+err); }
-                );
-
-    }
-    
- 
-    </script>
-
+        
     </head>
     
     <body>
         <?PHP include("app_header.php"); ?>
         
-        <div id="bodyContent">
-            <h1> Messages </h1>
-             
-            <table>
-                <tbody>
-                    <tr >
-                        <td id ="tbl_msg">
-                             
-                                <textarea id ="text_area"onfocus="ChangeSize(this);" off></textarea>
-                                
-                        </td>
-                    </tr>
-                    <tr>
-                        <td id ="tbl_tags">
-                            
-                                <input type="text" name="name_tag" id ="audience"/>
-                            
-                        </td>
-                    </tr>
-                    <tr>
-                        <td id ="tbl_buttons">
-                           
-                            <input type="submit" name="submit"  onclick="submitWhoop()" id ="whoop_butn"/>
-                                <input type="button" name="tag_button" onclick="show_tags()" id ="tag_butn"/>
-                                
-                            
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-
-            <br/><br/> 
-            <ul id="elementList">
-                <li>                 
-                    <img id="profile_pic" src="http://demo.tutorialzine.com/2009/09/making-our-own-twitter-timeline/img/avatar.jpg" width="48" height="48" alt="profile_pic" />
-                    <div id="msg_txt">
-                        <div id="user_name">Anthony Balmeo</div>
-                        Check out this new layout, its pretty awesome!
-                    </div>
-                    <div id ="edit_menu" >
-                         <a href="javascript:reply_msg()">Reply</a>
-                         <a href="javascript:share_msg()">Share</a>
-                         <a href="javascript:delete_msg()">Delete</a>
-                    </div>
-                    <div id="clear"></div>                      
-                </li>  
-            </ul>
-
-        <br/>
-        <br/>
-        <br/>
         
-        <?PHP include("app_footer.php"); ?> 
-        </div>      
+        <div id="bodyContent">
+        <h1> Messages </h1>
 
+        <div>
+        <?PHP include("app_footer.php"); ?>
     </body>
     
 </html>
